f54fed33b413e597a6965adec523e44d890db9f1
Dalma Tálas <talasdalmaalexandra@hotmail.com>
2023-03-14 21:45:21 +0100

    Breaking the code into three major parts Renaming some variables

--- AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode.cpp
@@ -5,117 +5,139 @@
 #include <cassert>
 #include <chrono>
 
-int main (int argc, char** argv)
+const std::string FileName = "day8.txt";
+
+std::vector<std::vector<int>> ReadForest (const std::string& fileName)
 {
-    auto startTime = std::chrono::steady_clock::now ();
-
-    std::ifstream inf ("day8.txt");
-    std::string l;
-    std::vector<std::vector<int>> m;
-    while (std::getline (inf, l)) {
-        std::vector<int> t;
-        for (int i = 0; i < l.size (); i++) {
-            t.push_back (l[i] - '0');
+    std::ifstream fileStream (fileName);
+    std::string line;
+    std::vector<std::vector<int>> forest;
+    while (std::getline (fileStream, line)) {
+        std::vector<int> row;
+        for (unsigned int i = 0; i < line.size (); i++) {
+            row.push_back (line[i] - '0');
         }
-        m.push_back (t);
+        forest.push_back (row);
     }
+    return forest;
+}
 
-    int vt = 0;
-    for (int i = 0; i < m.size (); i++) {
-        for (int j = 0; j < m[0].size (); j++) {
-            if (i == 0 || j == 0 || i == m.size () - 1 || j == m[0].size () - 1) {
-                vt++;
+
+int GetNumberOfVisibleTreesInForest (const std::vector<std::vector<int>>& forest)
+{
+    int visibleTreeCount = 0;
+    for (unsigned int i = 0; i < forest.size (); i++) {
+        for (unsigned int j = 0; j < forest[0].size (); j++) {
+            if (i == 0 || j == 0 || i == forest.size () - 1 || j == forest[0].size () - 1) {
+                visibleTreeCount++;
                 continue;
             }
             bool v = true;
-            for (int k = 0; k < i; k++) {
-                if (m[k][j] >= m[i][j]) {
+            for (unsigned int k = 0; k < i; k++) {
+                if (forest[k][j] >= forest[i][j]) {
                     v = false;
                     break;
                 }
             }
             if (v) {
-                vt++;
+                visibleTreeCount++;
                 continue;
             }
             v = true;
-            for (int k = m.size () - 1; k > i; k--) {
-                if (m[k][j] >= m[i][j]) {
+            for (unsigned int k = forest.size () - 1; k > i; k--) {
+                if (forest[k][j] >= forest[i][j]) {
                     v = false;
                     break;
                 }
             }
             if (v) {
-                vt++;
+                visibleTreeCount++;
                 continue;
             }
             v = true;
-            for (int k = 0; k < j; k++) {
-                if (m[i][k] >= m[i][j]) {
+            for (unsigned int k = 0; k < j; k++) {
+                if (forest[i][k] >= forest[i][j]) {
                     v = false;
                     break;
                 }
             }
             if (v) {
-                vt++;
+                visibleTreeCount++;
                 continue;
             }
             v = true;
-            for (int k = m[0].size () - 1; k > j; k--) {
-                if (m[i][k] >= m[i][j]) {
+            for (unsigned int k = forest[0].size () - 1; k > j; k--) {
+                if (forest[i][k] >= forest[i][j]) {
                     v = false;
                     break;
                 }
             }
             if (v)
-                vt++;
+                visibleTreeCount++;
         }
     }
-    std::cout << vt << std::endl;
+    return visibleTreeCount;
+}
 
-    assert (vt == 1792);
 
-    int mss = 0;
-    for (int i = 0; i < m.size (); i++) {
-        for (int j = 0; j < m[0].size (); j++) {
-            if (i == 0 || j == 0 || i == m.size () - 1 || j == m[0].size () - 1) {
+int GetHighestScenicScoreInForest (const std::vector<std::vector<int>>& forest)
+{
+    int maxScore = 0;
+    for (unsigned int i = 0; i < forest.size (); i++) {
+        for (unsigned int j = 0; j < forest[0].size (); j++) {
+            if (i == 0 || j == 0 || i == forest.size () - 1 || j == forest[0].size () - 1) {
                 continue;
             }
             int lss1 = 0;
             for (int k = i - 1; k >= 0; k--) {
                 lss1++;
-                if (m[k][j] >= m[i][j])
+                if (forest[k][j] >= forest[i][j])
                     break;
             }
             int lss2 = 0;
-            for (int k = i + 1; k < m.size (); k++) {
+            for (unsigned int k = i + 1; k < forest.size (); k++) {
                 lss2++;
-                if (m[k][j] >= m[i][j])
+                if (forest[k][j] >= forest[i][j])
                     break;
             }
             int lss3 = 0;
             for (int k = j - 1; k >= 0; k--) {
                 lss3++;
-                if (m[i][k] >= m[i][j])
+                if (forest[i][k] >= forest[i][j])
                     break;
             }
             int lss4 = 0;
-            for (int k = j + 1; k < m[0].size (); k++) {
+            for (unsigned int k = j + 1; k < forest[0].size (); k++) {
                 lss4++;
-                if (m[i][k] >= m[i][j])
+                if (forest[i][k] >= forest[i][j])
                     break;
             }
             int ss = lss1 * lss2 * lss3 * lss4;
-            if (ss > mss)
-                mss = ss;
+            if (ss > maxScore)
+                maxScore = ss;
         }
     }
-    std::cout << mss << std::endl;
+    return maxScore;
+}
 
+
+int main (int argc, char** argv)
+{
+    const auto startTime = std::chrono::steady_clock::now ();
+
+    const std::vector<std::vector<int>> m = ReadForest (FileName);
+
+    const int vt = GetNumberOfVisibleTreesInForest (m);
+    std::cout << vt << std::endl;
+    assert (vt == 1792);
+
+    const int mss = GetHighestScenicScoreInForest (m);
+    std::cout << mss << std::endl;
     assert (mss == 334880);
 
-    auto endTime = std::chrono::steady_clock::now ();
-    long runTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count (); 
+    const auto endTime = std::chrono::steady_clock::now ();
+    const auto runTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count (); 
+    std::cout << runTime << std::endl;
     assert (runTime < 200);
 
     return 0;
